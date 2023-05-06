031bf68107d4cc62f2a1a50ced23705aa9995e93
Ricardo Mejias Travieso <63120247+Ricardo-MT@users.noreply.github.com>
2023-05-06 18:20:57 +0200

    Flutter version

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:provider/provider.dart';
 import 'package:ricardomejiastravieso/providers/locale_provider.dart';
 import 'package:ricardomejiastravieso/utils/theme.dart';
 import 'package:ricardomejiastravieso/view/page/home.dart';
+import 'package:seo_renderer/helpers/renderer_state.dart';
+import 'package:seo_renderer/helpers/robot_detector_vm.dart';
 import 'package:url_strategy/url_strategy.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -14,7 +16,7 @@ import 'providers/theme_provider.dart';
 void main() {
   setPathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyProviders());
+  runApp(const RobotDetector(debug: true, child: MyProviders()));
 }
 
 class MyProviders extends StatelessWidget {
@@ -39,7 +41,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ricardo',
+      title: 'Ricardo Mejías Travieso | Full Stack & Mobile Developer',
+      navigatorObservers: [seoRouteObserver],
       localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
